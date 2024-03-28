3593add4d0ddc679501265b726204caa8a1f5a23
Sergio Udave <webteam@lemonadestand.org>
2024-03-28 16:32:59 -0500

    Update taxonomy-tour-category.php
    Created a new version of the tour cards and updated the shortcodes accordingly

--- taxonomy-tour-category.php
+++ taxonomy-tour-category.php
@@ -14,26 +14,33 @@ Template name: Tour Category Archive Page
 
         <?php
         $shortcodes = '';
-        $shortcodes .= '[row h_align="center"]';
-        $shortcodes .= '[col span__sm="12" color="light"]';
-        $shortcodes .= '[row_inner style="collapse" v_align="equal" h_align="center"]';
+
+        // Opening section
+        $shortcodes .= '[section]';
+        $shortcodes .= '[gap height="30px"]';
+
+        // Title with a paragraph
+        $shortcodes .= '[row]';
+        $shortcodes .= '[col span="8" span__sm="12" span__md="12"]';
+        $shortcodes .= '[row_inner h_align="center"]';
         $shortcodes .= '[col_inner span__sm="12" span__md="10"]';
-
-        $shortcodes .= '[gap height="55px"]';
-
         /*
-        $shortcodes .= '[row_inner_1 style="collapse"]';
-        $shortcodes .= '[col_inner_1 span="8" span__sm="12" span__md="12"]';
-        $shortcodes .= '[ux_text font_size="1.8" font_size__sm="1.5"]';
-        $shortcodes .= '<h1 class="mb-0">Lorem Ipsum</h1>';
+        $shortcodes .= '[ux_text font_size="1.4"]';
+        $shortcodes .= '<h1>This is a simple headline</h1>';
         $shortcodes .= '[/ux_text]';
-        $shortcodes .= '<p>Lorem ipsum dolor sit amet.</p>';
-        $shortcodes .= '[button text="See All Tours" style="link" link="/tours/"]';
-        $shortcodes .= '[/col_inner_1]';
-        $shortcodes .= '[/row_inner_1]';
+        $shortcodes .= '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>';
         */
+        $shortcodes .= '[button text="See All Tours" icon="icon-angle-right" link="/tours"]';
+        $shortcodes .= '[/col_inner]';
+        $shortcodes .= '[/row_inner]';
+        $shortcodes .= '[/col]';
+        $shortcodes .= '[/row]';
+        // End title with a paragraph 
 
-        $shortcodes .= '[row_inner_1 v_align="equal" h_align="center"]';
+        // Opening row/col/row for clickable cards and responsiveness
+        $shortcodes .= '[row h_align="center"]';
+        $shortcodes .= '[col span__sm="12" span__md="10"]';
+        $shortcodes .= '[row_inner v_align="equal"]';
         ?>
 
         <?php while (have_posts()) : the_post(); ?>
@@ -49,48 +56,49 @@ Template name: Tour Category Archive Page
             $ls_tours_year                 = date('Y', strtotime($ls_tours_start_date));
             $ls_tours_link                 = get_permalink(get_the_ID());
 
-            $shortcodes .= '[col_inner_1 label="HOME TOUR CARD" span="4" span__sm="12" span__md="10"]';
-            $shortcodes .= '[section bg="' . $ls_tours_hero_image . '" bg_size="original" bg_overlay="rgba(0, 0, 0, 0.55)" bg_pos="35% 7%" padding="0px"]';
-
-            $shortcodes .= '[ux_html label="Make Card Clickable"]';
+            $shortcodes .= '[col_inner span="4" span__sm="12" span__md="12" padding="100% 0px 0px 0px" class="clickable-card tour-card"]';
+            $shortcodes .= '[ux_html]';
             $shortcodes .= '<a href="' . $ls_tours_link . '" class="clickable-card-link"></a>';
             $shortcodes .= '[/ux_html]';
+            $shortcodes .= '[ux_image id="' . $ls_tours_hero_image . '" image_overlay="rgba(0, 0, 0, 0.55)" class="fill"]';
+            $shortcodes .= '[row_inner_1]';
+            $shortcodes .= '[col_inner_1 span__sm="12" padding="0px 0px 0px 30px" margin="-85% 0px 0px 0px"]';
 
-            $shortcodes .= '[row_inner_2 style="collapse" width="full-width" padding="45px 0px 30px 0px"]';
-            $shortcodes .= '[col_inner_2 span__sm="12"]';
+            if (!empty($ls_tours_start_date_month_day) && !empty($ls_tours_end_date_month_day)) {
+                $shortcodes .= '[ux_text font_size="1.1"]';
+                $shortcodes .= '<h6 class="mb-0">' . $ls_tours_start_date_month_day . ' - ' . $ls_tours_end_date_month_day . '</h6>';
+                $shortcodes .= '[/ux_text]';
+            }
 
-            $shortcodes .= '[ux_text font_size="1.1"]';
-            $shortcodes .= '<h6 class="mb-0">' . $ls_tours_start_date_month_day . ' - ' . $ls_tours_start_date_month_day . '</h6>';
-            $shortcodes .= '[/ux_text]';
+            if (!empty($ls_tours_year)) {
+                $shortcodes .= '[ux_text font_size="2.2" line_height="0.75"]';
+                $shortcodes .= '<h3 class="mb-0">' . $ls_tours_year . '</h3>';
+                $shortcodes .= '[/ux_text]';
+            }
 
-            $shortcodes .= '[ux_text font_size="2.2" line_height="0.75"]';
-            $shortcodes .= '<h3 class="mb-0">' . $ls_tours_year . '</h3>';
-            $shortcodes .= '[/ux_text]';
-
-            $shortcodes .= '[gap height="230px" height__sm="100px"]';
-
-            // If the tour is new, display a "New Tour" badge. Hidden by default.
-            $shortcodes .= '[ux_text font_size="0.8" text_color="rgb(0,0,0)" visibility="hidden"]';
-            $shortcodes .= '<p class="uppercase mb-half"><span style="background-color: #fff; padding:8px"><b>New Tour</b></span></p>';
-            $shortcodes .= '[/ux_text]';
-            
-            $shortcodes .= '<h3 class="mb-0">' . $ls_tours_name . '</h3>';
-            $shortcodes .= '<p>&nbsp;</p>';
-            $shortcodes .= '[button text="Learn More" icon="icon-angle-right" link="' . $ls_tours_link . '"]';
-            $shortcodes .= '[/col_inner_2]';
-            $shortcodes .= '[/row_inner_2]';
-
-            $shortcodes .= '[/section]';
             $shortcodes .= '[/col_inner_1]';
+            $shortcodes .= '[col_inner_1 span__sm="12" padding="0px 30px 0px 30px"]';
+
+            if (!empty($ls_tours_name)) {
+                $shortcodes .= '<h3 class="mb-0">' . $ls_tours_name . '</h3>';
+                $shortcodes .= '<p>&nbsp;</p>';
+                $shortcodes .= '[button text="Learn More" icon="icon-angle-right"]';
+            }
+
+            $shortcodes .= '[/col_inner_1]';
+            $shortcodes .= '[/row_inner_1]';
+            $shortcodes .= '[/col_inner]';
             ?>
         <?php endwhile; ?>
 
         <?php
-        $shortcodes .= '[/row_inner_1]';
-        $shortcodes .= '[/col_inner]';
+        // Close out the opening row/col/row for clickable cards and responsiveness
         $shortcodes .= '[/row_inner]';
         $shortcodes .= '[/col]';
         $shortcodes .= '[/row]';
+
+        // Close out the section
+        $shortcodes .= '[/section]';
         echo do_shortcode($shortcodes);
         ?>
 
