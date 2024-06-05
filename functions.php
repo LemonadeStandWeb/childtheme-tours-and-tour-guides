ea665174682deb412f01a475ad3d3fac1d552402
LemonadeStandWeb <134979133+LemonadeStandWeb@users.noreply.github.com>
2024-06-05 14:21:21 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,17 +12,3 @@ add_filter( 'admin_email_check_interval', '__return_false' );
 
 //Remove Gravity Forms Label "* Indicates Required"
 add_filter( 'gform_required_legend', '__return_empty_string' );
-
-// Adjust permalink structure for the custom post types
-function ls_tours_permalinks( $post_link, $post ) {
-    if ( is_object( $post ) && $post->post_type == 'tours' ) {
-        $terms = wp_get_object_terms( $post->ID, 'tour-category' );
-        if ( $terms ) {
-            return str_replace( '%tour-category%', $terms[0]->slug, $post_link );
-        } else {
-            return str_replace( '%tour-category%', 'uncategorized', $post_link );
-        }
-    }
-    return $post_link;
-}
-add_filter( 'post_type_link', 'ls_tours_permalinks', 1, 2 );
